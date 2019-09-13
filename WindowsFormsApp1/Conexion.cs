90048ced0bf5511104ed124971c63677b2b4b4cd
JuanPabloH <jpherrera69@gmail.com>
2019-09-13 08:03:49 -0500

    conexion local sql server windows app

---
+++ WindowsFormsApp1/Conexion.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp1
+{
+    public class Conexion
+    {
+        private SqlConnection conn = new SqlConnection("Server=localhost\\SQLEXPRESS;Database=prueba;Trusted_Connection=True;");
+
+        public SqlConnection abrirConexion()
+        {
+            if (conn.State == ConnectionState.Closed)
+                conn.Open();
+
+            return conn;
+        }
+
+        public SqlConnection cerrarConexion()
+        {
+            if (conn.State == ConnectionState.Open)
+                conn.Close();
+
+            return conn;
+        }
+
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        private Conexion conexion = new Conexion();    
+        SqlCommand comando = new SqlCommand();
+        
+
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string primero=pruebaCons()[0];
+            label1.Text = primero;
+        }
+        //Consultar BD
+        public List<String> pruebaCons()
+        {
+            List<string> resultado = new List<string>();
+            SqlDataReader leer;
+            comando.Connection = conexion.abrirConexion();
+            comando.CommandText = "SELECT nombre FROM ejemplo";
+            leer = comando.ExecuteReader();
+            while (leer.Read())
+            {
+                resultado.Add(Convert.ToString(leer["nombre"]));
+            }
+            
+            return resultado;
+        }
+    }
+}
